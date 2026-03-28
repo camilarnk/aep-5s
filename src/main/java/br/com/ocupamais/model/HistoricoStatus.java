b262387d29968c3c56156c2da193a953cf3868cf
Camila Reinke <reinkecamila@gmail.com>
2026-03-27 22:45:51 -0300

    feat: atributos e metodos HistoricoStatus

--- src/main/java/br/com/ocupamais/model/HistoricoStatus.java
+++ src/main/java/br/com/ocupamais/model/HistoricoStatus.java
@@ -1,6 +1,35 @@
 package br.com.ocupamais.model;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 
 public class HistoricoStatus implements Serializable {
+
+    private Status status;
+    private LocalDateTime data;
+    private String responsavel;
+    private String comentario;
+
+    public HistoricoStatus(Status status, String responsavel, String comentario) {
+        this.status = status;
+        this.data = LocalDateTime.now();
+        this.responsavel = responsavel;
+        this.comentario = comentario;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public String getResponsavel() {
+        return responsavel;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
 }
