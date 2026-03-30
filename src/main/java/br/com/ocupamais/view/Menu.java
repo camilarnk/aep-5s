1e0d696c423b1f1b4966cf199c420a8cfd7a5116
Camila Reinke <reinkecamila@gmail.com>
2026-03-30 16:29:55 -0300

    feat(Menu): implementa método para iniciar o sistema em CLI

--- src/main/java/br/com/ocupamais/view/Menu.java
+++ src/main/java/br/com/ocupamais/view/Menu.java
@@ -15,6 +15,37 @@ public class Menu {
     }
 
     public void iniciar() {
+        int perfil;
+
+        do {
+            System.out.println("---- BEM-VINDO AO OCUPAMAIS ----");
+            System.out.println("Como qual tipo de usuário deseja entrar?");
+            System.out.println("1- Cidadão");
+            System.out.println("2- Gestor");
+            System.out.println("0- Sair");
+
+            perfil = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(perfil) {
+                case 1 -> menuCidadao();
+                case 2 -> menuGestor();
+                case 0 -> System.out.println("Encerrando o Sistema...");
+                default -> System.out.println("Opção Inválida!");
+            }
+        } while(perfil != 0);
+    }
+
+    public void menuCidadao() {
+        System.out.println("O que deseja realizar?");
+        System.out.println("1- Criar Solicitação");
+        System.out.println("2- Buscar por Protocolo");
+
+        int op = scanner.nextInt();
+        scanner.nextLine();
+    }
+
+    public void menuGestor() {
 
     }
 }
