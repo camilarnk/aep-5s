278f12e62c89d7bc513c2776f2204dca9345f859
Camila Reinke <reinkecamila@gmail.com>
2026-04-01 16:57:06 -0300

    feat(Status): adiciona seleção por id

--- src/main/java/br/com/ocupamais/model/Status.java
+++ src/main/java/br/com/ocupamais/model/Status.java
@@ -1,9 +1,28 @@
 package br.com.ocupamais.model;
 
 public enum Status {
-    ABERTO,
-    TRIAGEM,
-    EM_EXECUCAO,
-    RESOLVIDO,
-    ENCERRADO
+    ABERTO(1),
+    TRIAGEM(2),
+    EM_EXECUCAO(3),
+    RESOLVIDO(4),
+    ENCERRADO(5);
+
+    private int id;
+
+    Status(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Status buscarPeloId(int id) {
+        for(Status status : values()) {
+            if(status.getId() == id) {
+                return status;
+            }
+        }
+        throw new IllegalArgumentException("ID de Status inválido: " + id);
+    }
 }
