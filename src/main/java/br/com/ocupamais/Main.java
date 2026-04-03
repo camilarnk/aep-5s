d3e4a10ce3222fb52bb67fc7d79d3438e119c9c2
Camila Reinke <reinkecamila@gmail.com>
2026-04-03 02:10:09 -0300

    feat(main): adiciona fluxo de interaçao

--- src/main/java/br/com/ocupamais/Main.java
+++ src/main/java/br/com/ocupamais/Main.java
@@ -1,7 +1,19 @@
 package br.com.ocupamais;
 
+import br.com.ocupamais.controller.SolicitacaoController;
+import br.com.ocupamais.repository.SolicitacaoRepository;
+import br.com.ocupamais.service.SolicitacaoService;
+import br.com.ocupamais.view.Menu;
+
 public class Main {
     public static void main(String[] args) {
+
+        SolicitacaoRepository repository = new SolicitacaoRepository();
+        SolicitacaoService service = new SolicitacaoService(repository);
+        SolicitacaoController controller = new SolicitacaoController(service);
+        Menu menu = new Menu(controller);
+
+        menu.iniciar();
 
     }
 }
