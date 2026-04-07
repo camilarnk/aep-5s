4b619fed808fc6cf44368774585f8f037628b06b
Camila Reinke <reinkecamila@gmail.com>
2026-04-07 16:56:11 -0300

    feat: opção por ID para enum Prioridade e Categoria

--- src/main/java/br/com/ocupamais/model/Categoria.java
+++ src/main/java/br/com/ocupamais/model/Categoria.java
@@ -1,11 +1,36 @@
 package br.com.ocupamais.model;
 
 public enum Categoria {
-    ILUMINACAO,
-    BURACO,
-    LIMPEZA,
-    SAUDE,
-    SEGURANCA,
-    EDUCACAO,
-    OUTROS
+    ILUMINACAO(1),
+    BURACO(2),
+    LIMPEZA(3),
+    SAUDE(4),
+    SEGURANCA(5),
+    EDUCACAO(6),
+    OUTROS(7);
+
+    private int id;
+
+    Categoria(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Categoria buscarPeloId(int id) {
+        for(Categoria categoria : values()) {
+            if(categoria.getId() == id) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("ID de Categoria inválido " + id);
+    }
+
+    public static void exibirOpcoes() {
+        for (Categoria categoria : values()) {
+            System.out.println(categoria.getId() + " - " + categoria);
+        }
+    }
 }
--- src/main/java/br/com/ocupamais/model/Prioridade.java
+++ src/main/java/br/com/ocupamais/model/Prioridade.java
@@ -1,7 +1,32 @@
 package br.com.ocupamais.model;
 
 public enum Prioridade {
-    BAIXA,
-    MEDIA,
-    ALTA
+    BAIXA(1),
+    MEDIA(2),
+    ALTA(3);
+
+    private int id;
+
+    Prioridade(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public static Prioridade buscarPeloId(int id) {
+        for (Prioridade prioridade : values()) {
+            if (prioridade.getId() == id) {
+                return prioridade;
+            }
+        }
+        throw new IllegalArgumentException("ID inválido");
+    }
+
+    public static void exibirOpcoes() {
+        for (Prioridade prioridade : values()) {
+            System.out.println(prioridade.getId() + " - " + prioridade);
+        }
+    }
 }
