9427d80fc1dde42a636674267feed53288072b7c
Camila Reinke <reinkecamila@gmail.com>
2026-06-06 17:13:38 -0300

    feat: html passo 1/criar nova solicitacao

--- src/main/java/br/com/ocupamais/controller/SolicitacaoController.java
+++ src/main/java/br/com/ocupamais/controller/SolicitacaoController.java
@@ -1,14 +1,8 @@
 package br.com.ocupamais.controller;
-
-import br.com.ocupamais.model.Categoria;
-import br.com.ocupamais.model.Prioridade;
-import br.com.ocupamais.model.Solicitacao;
-import br.com.ocupamais.model.Status;
 import br.com.ocupamais.service.SolicitacaoService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/solicitacoes")
@@ -20,43 +14,9 @@ public class SolicitacaoController {
         this.service = service;
     }
 
-    public Solicitacao criarSolicitacao(String descricao, String localizacao,
-                                        Categoria categoria, Prioridade prioridade,
-                                        boolean anonimo, String nomeSolicitante) {
-
-        return service.criarSolicitacao(descricao, localizacao, categoria, prioridade,
-                anonimo, nomeSolicitante);
-    }
-
-    public List<Solicitacao> listarSolicitacoes() {
-        return service.listarSolicitacoes();
-    }
-
-    public List<Solicitacao> listarSolicitacoesAbertas() {
-        return service.listarSolicitacoesAbertas();
-    }
-
-    public List<Solicitacao> filtrarPorCategoria(Categoria categoria) {
-        return service.filtrarPorCategoria(categoria);
-    }
-
-    public List<Solicitacao> filtrarPorPrioridade(Prioridade prioridade) {
-        return service.filtrarPorPrioridade(prioridade);
-    }
-
-    public List<Solicitacao> filtrarPorLocalizacao(String localizacao) {
-        return service.filtrarPorLocalizacao(localizacao);
-    }
-
-    public Solicitacao buscarPorProtocolo(String protocolo) {
-        return service.buscarPorProtocolo(protocolo);
-    }
-
-    public void atualizarStatus(String protocolo, Status status,
-                                String responsavel, String comentario,
-                                String justificativa) {
-
-        service.atualizarStatus(protocolo, status, responsavel, comentario, justificativa);
+    @GetMapping("/nova")
+    public String novaSolicitacao() {
+        return "solicitacoes/nova-solicitacao";
     }
 
 }
