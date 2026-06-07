23254de33434bf5c44fdbe5e943a033a73407e32
Camila Reinke <reinkecamila@gmail.com>
2026-06-07 00:14:05 -0300

    feat: endpoints get e post para buscar solicitacao

--- src/main/java/br/com/ocupamais/controller/SolicitacaoController.java
+++ src/main/java/br/com/ocupamais/controller/SolicitacaoController.java
@@ -25,6 +25,11 @@ public class SolicitacaoController {
         return "solicitacoes/nova-solicitacao";
     }
 
+    @GetMapping("/acompanhar")
+    public String acompanharSolicitacao() {
+        return "solicitacoes/acompanhar";
+    }
+
     @PostMapping
     public String salvarSolicitacao(
             @RequestParam String descricao,
@@ -52,6 +57,21 @@ public class SolicitacaoController {
         model.addAttribute("protocolo", solicitacao.getProtocolo());
 
         return "solicitacoes/sucesso";
+    }
+
+    @PostMapping("/acompanhar")
+    public String buscarSolicitacao(@RequestParam String protocolo, Model model) {
+        Solicitacao solicitacao = service.buscarPorProtocolo(protocolo);
+
+        if (solicitacao == null) {
+            model.addAttribute("erro",
+                    "Nenhuma solicitação encontrada para esse protocolo.");
+            return "solicitacoes/acompanhar";
+        }
+
+        model.addAttribute("solicitacao", solicitacao);
+
+        return "solicitacoes/acompanhar";
     }
 
 }
